a97cb0baa871c5af8daed814e80808e1e652e9a1
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-04-08 19:42:45 -0400

    maps

--- app/controllers/gmap_controller.rb
+++ app/controllers/gmap_controller.rb
@@ -1,5 +1,5 @@
-class GsearchController < ApplicationController
-    def new
+class GmapController < ApplicationController
+    def map
         redirect_to root_path if session[:user_id] == nil
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   post 'signup', to: 'account#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
-
+  get 'gmap', to:'gmap#map', as: 'gmap'
 
   get '/auth/:provider/callback' => 'sessions#omniauth'
 end
