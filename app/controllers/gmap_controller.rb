890442be90865e183d318510aae7c9640f24928b
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-04-18 01:23:39 -0400

    commit

--- app/controllers/gmap_controller.rb
+++ app/controllers/gmap_controller.rb
@@ -3,19 +3,40 @@ class GmapController < ApplicationController
         redirect_to root_path if session[:user_id] == nil
         @search = []
         data = JSON.load(open("https://ipapi.co/json/"))
-        location = Gmaps.new({:account_id => session[:user_id], :latitude => data["latitude"], :longitude => data["longitude"]})
-        location.save
+        my_loc = Gmaps.find_by(account_id: session[:user_id])
+        if my_loc
+            my_loc.latitude = data["latitude"]
+            my_loc.longitude = data["longitude"]
+            my_loc.save
+        else
+            new_loc = Gmaps.new({:account_id => session[:user_id], :latitude => data["latitude"], :longitude => data["longitude"]})
+            new_loc.save
+        end
     end
 
     def results
         @search = []
-        places = Geocoder.search(params[:search]).each do |place|
+        @input = params[:search]
+        @client = GooglePlaces::Client.new('AIzaSyCo_oi77Myi1Atxai8-Ocz9gdFV3upyPjU')
+        @client.spots_by_query(params[:search]).each do |place|
             plc = {
-                "name" => place.data["display_name"],
-                "latitude" => place.data["lat"].to_f,
-                "longitude" => place.data["lon"].to_f
+                "name" => place.json_result_object["name"],
+                "latitude" => place.json_result_object["geometry"]["location"]["lat"],
+                "longitude" => place.json_result_object["geometry"]["location"]["lng"]
             }
             @search << plc
+        end
+        if @search == []
+            Business.all.each do |bizz|
+                if bizz.name == params[:search]
+                    biz = {
+                        "name" => bizz.name,
+                        "latitude" => bizz.latitude.to_f,
+                        "longitude" => bizz.longitude.to_f
+                    }
+                    @search << biz
+                end
+            end
         end
         render "map"
     end
