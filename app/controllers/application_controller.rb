604824b5981a052bfe665340475b8d50093469a0
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-07-09 19:53:04 -0400

    Updated Current User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user
+  
+    def current_user
+        if session[:user_id]
+            @user ||= Account.find(session[:user_id]) if session[:user_id]
+        end 
+    end
 end
